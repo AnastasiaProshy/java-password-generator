cd7012612232b4972a58549f370b3748c44cc43e
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-06-09 18:27:12 +0200

    add: info collector
    name & surname & color & birthday day+month+year

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -9,7 +9,20 @@ public class PasswordGenerator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner noteDown = new Scanner(System.in); //scanner assignment
+		
+		//name & surname & color
+		System.out.println("Please type your name:");
+		String userName = noteDown.nextLine();
+		
+		System.out.println("your surname:");
+		String userSurname = noteDown.nextLine();
 
+		System.out.println("your favorite color:");
+		String userColor = noteDown.nextLine();
+		
+		//birthday day+month+year
+			System.out.println("Please type the day of your birth dd/mm/yyyy:");
+			String userBirth = noteDown.nextLine();
 	}
 
 }
