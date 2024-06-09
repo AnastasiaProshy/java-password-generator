408cdcd93aac7861776b12aa6e939bc23308785f
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-06-09 17:58:06 +0200

    Useful library & git-ignore
    Imported: scanner, date formatter & organizer; .gitignore file.

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,5 +1,9 @@
 package org.lessons.java.security;
 
+import java.time.LocalDate; //date organizer
+import java.time.format.DateTimeFormatter; //date formatter
+import java.util.Scanner; //type scanner 
+
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
